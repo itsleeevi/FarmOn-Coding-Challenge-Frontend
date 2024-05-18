8095a538f190f4e33f018ebbd4459c4f9d91f052
leeevi <leventekecskemeti@gmail.com>
2024-05-18 02:30:23 +0200

    first

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -25,7 +25,13 @@ const SearchParcel = () => {
 
 const Home: React.FC = () => {
   return (
-    <Suspense fallback={<Spinner />}>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <Spinner />
+        </div>
+      }
+    >
       <SearchParcel />
     </Suspense>
   );
